e122f72feabce479e7e88cf3244068c9e6a5762b
jksun <jksun@thoughtworks.com>
2014-06-25 11:51:13 +0800

    add create price and product test

--- api/src/main/java/org/thoughtworks/com/provider/ProductRepository.java
+++ api/src/main/java/org/thoughtworks/com/provider/ProductRepository.java
@@ -1,14 +1,15 @@
 package org.thoughtworks.com.provider;
 
+import org.apache.ibatis.annotations.Param;
 import org.thoughtworks.com.domain.Price;
 import org.thoughtworks.com.domain.Product;
 
 public interface ProductRepository {
     public Product getProductById(int productId);
 
-    public int createProduct(Product product);
+    public int createProduct(@Param("product") Product product);
 
     public Price getPriceById(int priceId);
 
-    public int createProductPrice(Product product, Price price);
+    public int createProductPrice(@Param("product") Product product, @Param("price") Price price);
 }
--- api/src/test/java/provider/ProductRepositoryTest.java
+++ api/src/test/java/provider/ProductRepositoryTest.java
@@ -31,4 +31,25 @@ public class ProductRepositoryTest {
         Price price = productRepository.getPriceById(1);
         assertThat(price.getId(), is(1));
     }
+
+    @Test
+    public void should_create_price_for_product() throws IOException {
+        MyBatisExecuter myBatisExecuter = new MyBatisExecuter();
+        SqlSession session = myBatisExecuter.getSession();
+        ProductRepository productRepository = session.getMapper(ProductRepository.class);
+        Price price = new Price(2.0);
+        productRepository.createProductPrice(new Product(1, "productName"), price);
+        assertThat(price.getId()>3, is(true));
+    }
+
+    @Test
+    public void should_create_product() throws IOException {
+        MyBatisExecuter myBatisExecuter = new MyBatisExecuter();
+        SqlSession session = myBatisExecuter.getSession();
+        ProductRepository productRepository = session.getMapper(ProductRepository.class);
+        Product product = new Product("productName");
+        productRepository.createProduct(product);
+        assertThat(product.getId()>3, is(true));
+    }
+
 }
