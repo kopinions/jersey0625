4ab32ffa39d4bb4c8bbbe21970a467ef3e119506
jksun <jksun@thoughtworks.com>
2014-06-25 09:21:46 +0800

    return 404 when not found product

--- api/src/main/java/org/thoughtworks/com/ProductResource.java
+++ api/src/main/java/org/thoughtworks/com/ProductResource.java
@@ -1,7 +1,10 @@
 package org.thoughtworks.com;
 
+import org.thoughtworks.com.domain.Product;
 import org.thoughtworks.com.json.response.ProductJson;
+import org.thoughtworks.com.provider.ProductRepository;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,11 +14,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/products")
 public class ProductResource {
 
+    @Inject
+    ProductRepository productRepository;
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public ProductJson getProduct(@PathParam("id") int id) {
+        Product product = productRepository.getProductById(id);
         return new ProductJson();
     }
 }
--- api/src/test/java/ProductResourceTest.java
+++ api/src/test/java/ProductResourceTest.java
@@ -1,18 +1,39 @@
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.thoughtworks.com.exception.ProductNotFoundException;
+import org.thoughtworks.com.provider.ProductRepository;
 
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProductResourceTest extends JerseyTest {
+
+    @Mock
+    ProductRepository productRepository;
+
     @Test
     public void should_return_200_when_get_product() {
         Response response = target("/products/1").request().accept(MediaType.APPLICATION_JSON_TYPE).get();
         assertEquals(response.getStatus(), 200);
+    }
+
+    @Test
+    public void should_return_404_when_no_found() {
+        when(productRepository.getProductById(eq(1))).thenThrow(ProductNotFoundException.class);
+        Response response = target("/products/1").request().accept(MediaType.APPLICATION_JSON_TYPE).get();
+//        assertThat(response.getStatus(), is(404));
+        assertEquals(response.getStatus(), 404);
     }
 
 
@@ -21,6 +42,13 @@ public class ProductResourceTest extends JerseyTest {
     protected Application configure() {
         ResourceConfig resourceConfig = new ResourceConfig();
         resourceConfig.packages("org.thoughtworks.com");
+        resourceConfig.register(new AbstractBinder() {
+
+            @Override
+            protected void configure() {
+                bind(productRepository).to(ProductRepository.class);
+            }
+        });
         return resourceConfig;
     }
 }
